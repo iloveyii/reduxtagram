666032750d34562c85261158467a2271cc0f2e04
Ali <iloveyii@yahoo.com>
2017-09-02 23:40:14 +0200

    use reducer, single page done

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Photo from './Photo';
 
 const Single = React.createClass({
     render() {
+        const i = this.props.posts.findIndex(post => post.code === this.props.params.postId);
+
         return(
             <div className="single-photo">
-                It's the single photo
+                <Photo post={this.props.posts[i]} key={i} i={i} {...this.props} />
             </div>
         )
     }
